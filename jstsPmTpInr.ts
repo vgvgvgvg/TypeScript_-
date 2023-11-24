c7a6cc10aacea3ff12c13d00e23b6174d9a4eb8a
Volodymyr <v_arish@ukr.net>
2023-11-24 08:34:43 +0200

    Кортежи в TypeScript

---
+++ jstsPmTpInr.ts
@@ -0,0 +1,12 @@
+// №1
+
+let year_month: [number, number];
+
+year_month = [2023, 11];
+console.log(year_month);
+
+
+// №2
+let date_tuple: [number, string];
+// №3
+let timeFrame: [number, number, number];
