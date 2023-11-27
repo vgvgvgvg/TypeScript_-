d38e156cfe18dc1ecefb0641c37dac719723b6a1
Volodymyr <v_arish@ukr.net>
2023-11-27 17:24:39 +0200

    Деструктуризация кортежа в TypeScript

---
+++ jstsPmTpDst.js
@@ -0,0 +1,6 @@
+// №1
+var time = [12, 59, 59];
+var hours = time[0], minutes = time[1], seconds = time[2];
+console.log(hours);
+console.log(minutes);
+console.log(seconds);
---
+++ jstsPmTpDst.ts
@@ -0,0 +1,7 @@
+// №1
+let time: [number, number, number] = [12, 59, 59];
+let [hours, minutes, seconds] = time;
+console.log(hours);
+console.log(minutes);
+console.log(seconds);
+
