c5af758196a0b352349ab30d1aac5bf4442ffa5f
Volodymyr <v_arish@ukr.net>
2023-11-29 18:40:14 +0200

    Строковые перечисления в TypeScript

---
+++ jstsPmEnSt.ts
@@ -0,0 +1,34 @@
+// №1
+enum Week {
+    Monday = 'Понедельник', 
+    Tuesday = 'Вторник', 
+    Wednesday = 'Среда', 
+    Thursday = 'Четверг',  
+    Friday = 'Пятница', 
+    Saturday = 'Суббота', 
+    Sunday = 'Воскресенье'
+};
+
+
+// №2
+
+enum Months { 
+    January = 'Январь',
+    February = 'Февраль', 
+    March = 'Март', 
+    April = 'Апрель', 
+    May = 'Май', 
+    June = 'Июнь', 
+    July = 'Июль', 
+    August = 'Август', 
+    September = 'Сентябрь', 
+    October = 'Октябрь',  
+    November = 'Ноябрь', 
+    December = 'Ноябрь'
+};
+
+
+
+
+
+
