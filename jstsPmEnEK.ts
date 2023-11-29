b9fd50dfebc785ab99a0ef60aea84966446c8b02
Volodymyr <v_arish@ukr.net>
2023-11-29 15:23:40 +0200

    Явные ключи перечислений в TypeScript

---
+++ jstsPmEnEK.ts
@@ -0,0 +1,9 @@
+// №1
+enum Months { January = 1, February = 2, March = 3, April = 4, May = 5, June = 6, July = 7, August = 8, September = 9, October = 10, November = 11, December = 12 };
+
+let month_01: Months = Months[1];
+let month_05: Months = Months[5];
+console.log(month_01, month_05);
+
+
+
