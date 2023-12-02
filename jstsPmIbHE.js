555533d1118e1056094b84d395f305afad4ccdb0
Volodymyr <v_arish@ukr.net>
2023-12-02 08:50:58 +0200

    Тип HTMLElement в TypeScript

---
+++ jstsPmIbHE.js
@@ -0,0 +1,8 @@
+// №2
+/*
+let input: HTMLElement = document.querySelector('#button');
+console.log(input);
+*/
+// №3
+var list = document.querySelectorAll('.list li');
+console.log(list[0]);
---
+++ jstsPmIbHE.ts
@@ -0,0 +1,8 @@
+// №2
+/* 
+let input: HTMLElement = document.querySelector('#button');
+console.log(input);
+*/
+// №3
+let list: HTMLHRElement = document.querySelectorAll('.list li');
+console.log(list[0]);
